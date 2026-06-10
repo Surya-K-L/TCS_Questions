6133fd546fac5a27a3096d96b6ab46fa5c24b040
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2026-06-10 21:22:23 +0530

    Create ASCIIValue.java

---
+++ Strings/ASCIIValue.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ASCIIValue{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char t=sc.next().charAt(0);
+        int p=t;
+        System.out.print(p);
+    }
+}
